07cd7da891ad68c85087791b058c3e09d0fec704
Sergii Gatezh <2880401+gatezh@users.noreply.github.com>
2025-06-09 13:43:53 -0600

    Test worker

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return new Response('Hello from my OpenAI API Worker');
 	},
 } satisfies ExportedHandler<Env>;
